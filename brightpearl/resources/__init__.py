6a82e555ede56c462dbf42ef04d69df11a70220a
Yashaskara Jois K R <43381463+Yashaskara@users.noreply.github.com>
2023-12-08 09:14:20 +0530

    Update __init__.py

--- brightpearl/resources/__init__.py
+++ brightpearl/resources/__init__.py
@@ -11,3 +11,6 @@ from brightpearl.resources.price_list import PriceList # noqa F401
 from brightpearl.resources.product_price import ProductPrice # noqa F401
 from brightpearl.resources.product_supplier import ProductSupplier # noqa F401
 from brightpearl.resources.product_primary_supplier import ProductPrimarySupplier # noqa F401
+from brightpearl.resources.warehouse import Warehouse
+from brightpearl.resources.stock_correction import StockCorrection
+from brightpearl.resources.location import Location
