59c0fc325700aeca0443903a11656e36cffe9a09
Yashwal S Kanchan <92976414+yashwal@users.noreply.github.com>
2024-01-08 19:40:24 +0530

    Update warehouse_defaults.py

--- brightpearl/resources/warehouse_defaults.py
+++ brightpearl/resources/warehouse_defaults.py
@@ -11,3 +11,9 @@ class WarehouseDefaults(object):
             "{}/{}/{}/{}/{}".format(self.resource_parent, warehouse_get, str(warehouse_id), "product", bp_id), "PUT",
             data=reqObj
         )
+
+    def get_default_location(self, warehouse_id=None, product_id = None):
+        warehouse_get = "warehouse"
+        return self.connection.make_request(
+            "{}/{}/{}/{}/{}".format(self.resource_parent, warehouse_get, warehouse_id,"location","default"), "GET", {}
+        )
